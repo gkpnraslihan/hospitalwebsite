7fa7cad62a0101fbec02240c3faf1968dc2e8a31
gkpnraslihan <gkpnrasli@gmail.com>
2023-07-05 16:18:13 +0300

    proje başlangıçı

---
+++ HospitalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Baslik = "Ana Sayfa";
+            ViewBag.AnaSayfa = "active";
+            return View();
+        }
+        public ActionResult Communication()
+        {
+            ViewBag.Baslik = "İletişim Sayfası";
+            ViewBag.Communication = "active";
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ HospitalWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HospitalWebsite.MvcApplication" Language="C#" %>
